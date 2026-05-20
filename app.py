6600a57a6a0540dbbd5900b8eaa325c57e28bc31
mlimamartins <limonlimon2705@gmail.com>
2026-05-20 18:55:13 -0300

    Corrige dependencias do deploy

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from PIL import Image
 import pytesseract
 from pdf2image import convert_from_path
 import re
+import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = (
     r"C:\Program Files\Tesseract-OCR\tesseract.exe"
